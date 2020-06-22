36058a949b7f4e54da30963a1180f17dce49cfb9
dgorski <d.gorski@fibargroup.com>
2020-06-22 18:23:15 +0200

    added api request

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Działa</h1>
-    </div>
-  );
+const API = 'https://api.openweathermap.org/data/2.5/weather?q=Turek&appid=e53dc0a789987740a8850b0150589346&units=metric'
+
+class App extends Component {
+  state={
+    cityName: 'Turek',
+    cityWeather: []
+  }
+
+  handleGetWeather = ()=>{
+    fetch(API)
+    .then(resp => (resp.json()))
+    .then(resp => (
+      this.setState({
+        cityWeather: resp.main
+      })
+    ))
+    .then(console.log(this.state.cityWeather))
+  }
+
+  
+  componentDidMount() {
+    this.handleGetWeather()
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <h1>Pogoda</h1>
+        <button onClick={this.handleGetWeather}>Poka pogode dla Turku</button>
+      </div>
+    );
+  }
 }
 
 export default App;
